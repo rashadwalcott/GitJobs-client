c28fa2b842925d465ccb6c0ae513f345dd6017f8
Rashad <rashad.walcott15@gmail.com>
2019-09-10 16:57:47 -0400

    Being Careful

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -8,11 +8,11 @@ export default class LandingPage extends Component {
   render(){
     return(
       <div className={landingpage}>
-      <h1>Welcome to GitJobs or Die Pryin'</h1>
+      <h1>Welcome to GitJobs or Die Pryin</h1>
 
         <p><Link to='/signup'>Sign Up</Link> <Link to='/login'>Login</Link></p>
 
-        <img src={logo} alt='git rich logo'/>
+        <img src= {logo} alt='git rich logo'/>
 
       </div>
     )
