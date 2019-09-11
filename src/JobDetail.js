cfd1b24b6737fe65dfda95dd2849ebd20e040e26
Rashad <rashad.walcott15@gmail.com>
2019-09-11 13:43:52 -0400

    React Parser added

--- src/JobDetail.js
+++ src/JobDetail.js
@@ -1,18 +1,17 @@
 import React, {Component} from 'react'
 import {jobdetail} from './JobDetail.module.scss'
+import ReactHtmlParser from 'react-html-parser'
 
 class JobDetail extends Component {
 
   render(){
     const { title, location, company, description } = this.props.job
-    const regex = /(<([^>]+)>)/ig;
-    const parsedDescription = description.replace(regex, '');
     return(
       <div className={jobdetail}>
       <p>{title}</p>
       <p>{location}</p>
       <p>{company}</p>
-      <p>{parsedDescription}</p>
+      <p>{ReactHtmlParser(description)}</p>
       </div>
     )
   }
